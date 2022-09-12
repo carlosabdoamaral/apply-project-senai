daa1ff4ac2a359f7532c97285cb8681e75db5929
camilagalieta <110790502+camilagalieta@users.noreply.github.com>
2022-09-11 21:52:31 -0300

    Update CandidatoXPesquisaController.java

--- Programação-Orientada-a-Objetos/Entrega-03/controller/CandidatoXPesquisaController.java
+++ Programação-Orientada-a-Objetos/Entrega-03/controller/CandidatoXPesquisaController.java
@@ -8,30 +8,29 @@ import model.CandidatoXPesquisa;
 import model.Pesquisa;
 
 public class CandidatoXPesquisaController {
-	
+
 	public void salvar(Pesquisa pesquisa, Candidato candidato, CandidatoXPesquisa cxp) throws Exception {
 		if (cxp.getCandidato() == null || cxp.getPesquisa() == null || cxp.getVotos() == 0) {
-			throw new Exception("Um ou mais atributos faltando!"); 
+			throw new Exception("Um ou mais atributos faltando!");
 		}
 		CandidatoXPesquisaDao.getInstance().salvar(pesquisa, candidato, cxp);
 	}
-	
+
 	public void atualizar(CandidatoXPesquisa cxp) throws Exception {
 		if (cxp.getCandidato() == null || cxp.getPesquisa() == null || cxp.getVotos() == 0) {
 			throw new Exception("Um ou mais atributos inválidos!");
 		}
-		CandidatoXPesquisaDao.getInstance().atualizar(cxp); 
+		CandidatoXPesquisaDao.getInstance().atualizar(cxp);
 	}
-	
+
 	public void excluir(int idCxp) throws Exception {
 		if (idCxp == 0) {
 			throw new Exception("Nenhum item selecionado!");
 		}
 		CandidatoXPesquisaDao.getInstance().excluir(idCxp);
 	}
-	
-	public List<CandidatoXPesquisa> listar(){
+
+	public List<CandidatoXPesquisa> listar() {
 		return CandidatoXPesquisaDao.getInstance().listar();
 	}
-
 }
